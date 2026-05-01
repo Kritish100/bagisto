9d35ccd081a269a02a67bed7c11e6962608595bb
Kritish <kritish073bct@ioepc.edu.np>
2026-05-01 17:12:45 +0545

    updated google tag manager

--- packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/index.blade.php
@@ -12,24 +12,13 @@
 >
     <head>
 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LRFFWDDSF2"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-LRFFWDDSF2');
-        </script>
-
-        <!-- Microsoft Clarity Script -->
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wk9lqj9wj8");
-        </script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-52XDVMPD');</script>
+        <!-- End Google Tag Manager -->       
 
         {!! view_render_event('bagisto.shop.layout.head.before') !!}
 
@@ -114,6 +103,13 @@
     
 
     <body>
+
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52XDVMPD"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
+
         {!! view_render_event('bagisto.shop.layout.body.before') !!}
 
         <a
